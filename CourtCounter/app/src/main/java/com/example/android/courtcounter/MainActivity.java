9f7aca75e5ac40ad734a84c1486d973ae797157e
Michelle <michelle200med@gmail.com>
2017-09-18 11:12:05 -0400

    Add files via upload

---
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.android.courtcounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void plus1RunA(View view) {
+        displayForTeamA(scoreTeamA += 1);
+    }
+
+    public void plus1OutA(View view) {
+        displayForTeamA(scoreTeamA += 1);
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void plus1RunB(View view) {
+        displayForTeamB(scoreTeamB += 1);
+    }
+
+    public void plus1OutB(View view) {
+        displayForTeamB(scoreTeamB +=1);
+    }
+
+    /**Resets scores to Zero*/
+    public void resetScore(View v) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+}
